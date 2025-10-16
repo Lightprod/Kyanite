56d9c38cd5049efb5c1bf9411eba22bc6869b4b2
Lightprod <Lightprod@outlook.fr>
2025-10-16 18:19:01 +0200

    cleanup build file and add step scripts to build

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
+log() { # from AmyOS's build files
+  echo "=== $* ==="
+}
+
+display() {
+    echo ""
+    echo "============= Content of $* ==============="
+    cat $*
+    echo "==========================================
+    "
+}
+
 set -ouex pipefail
 
-### Install packages
+log "Building $IMAGE_NAME"
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+log "Step 1 : Setting up image info metadata"
+    /ctx/steps/01-image-info.sh
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+log "Step 2: Native packages management"
+    /ctx/steps/02-native-packages.sh
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
-
-systemctl enable podman.socket
+log "Step 3: System configuration"
+    /ctx/steps/03-system-config.sh
