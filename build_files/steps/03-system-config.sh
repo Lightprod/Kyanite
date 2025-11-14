2b4ecd06d5e191dbeec4cee537bc8a01dcc3cd74
Lightprod <Lightprod@outlook.fr>
2025-11-14 19:45:50 +0100

    Add post-rebase setup script

--- build_files/steps/03-system-config.sh
+++ build_files/steps/03-system-config.sh
@@ -18,7 +18,7 @@ set -ouex pipefail
 # Defining variables
 
 DEFAULT_BASHRC_FILE=/etc/skel/.bashrc
-
+SCRIPT_FOLDER=/usr/libexec/kyanite
 
 # ====================================================================
 # Add init scripts
@@ -31,3 +31,19 @@ log "Add atuin init script to bash"
 
 echo 'eval "$(atuin init bash)"' >> $DEFAULT_BASHRC_FILE
 
+
+# ====================================================================
+# Copied scripts can't be executed, this fix it
+
+log "Fix scripts not being executable"
+
+cd ${SCRIPT_FOLDER}
+chmod +x *
+
+
+
+# ====================================================================
+#  Enable new systemd services
+
+log "Enabling setup service"
+systemctl enable kyanite-setup
